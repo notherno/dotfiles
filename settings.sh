6385572d96ff8ce9631463406653d14f6e232401
Kitano Takaaki <kitanow.takaaki@gmail.com>
2015-08-15 11:25:03 +0900

    settings.sh: git cloning script

--- settings.sh
+++ settings.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+git config --global user.name "Takaaki Kitano"
+git config --global user.email "kitanow.takaaki@gmail.com"
 git config --global color.ui auto
+
+if [ ! -e "~/src/dircolors-solarized" ] ; then
+    git clone https://github.com/seebi/dircolors-solarized
+fi
+
+if [ ! -e "~/.vim/bundle" ] ; then
+    mkdir -p ~/.vim/bundle
+    git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+fi
+
