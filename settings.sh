213a54f59b0d61fe22af76a4d782ff4c51f90fb7
Takaaki Kitano <kitanow.takaaki@gmail.com>
2015-08-15 11:34:01 +0900

    chmod setting.sh and modified conditional

--- settings.sh
+++ settings.sh
@@ -4,11 +4,11 @@ git config --global user.name "Takaaki Kitano"
 git config --global user.email "kitanow.takaaki@gmail.com"
 git config --global color.ui auto
 
-if [ ! -e "~/src/dircolors-solarized" ] ; then
+if [ ! -e "$HOME/src/dircolors-solarized" ] ; then
     git clone https://github.com/seebi/dircolors-solarized
 fi
 
-if [ ! -e "~/.vim/bundle" ] ; then
+if [ ! -e "$HOME/.vim/bundle" ] ; then
     mkdir -p ~/.vim/bundle
     git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 fi
